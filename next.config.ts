1b9b7a365a238a14c06054893258404dd0adde6e
D. Rushikesh <rikki0501hanuman@gmail.com>
2026-05-31 23:38:15 +0530

    chore: configure Next.js static export for GitHub Pages deployment and setup auto-deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/portfolioRK",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "raw.githubusercontent.com" },
     ],
